0caa7c8186409ab2ec3afeacb320468d280e0eab
vagrant <vagrant@localhost.localdomain>
2015-08-13 06:05:59 +0000

    version 1.10 - now supporting solr 5+

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,7 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Symfony\\Component\\EventDispatcher\\' => array($vendorDir . '/symfony/event-dispatcher'),
     'Solarium\\' => array($vendorDir . '/solarium/solarium/library'),
     'HTMLPurifier' => array($vendorDir . '/ezyang/htmlpurifier/library'),
 );
